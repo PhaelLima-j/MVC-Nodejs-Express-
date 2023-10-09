fb9de9e1c6726a904b318039bce7754ab750e515
Raphael Lima <rapha.lima.rl16@gmail.com>
2023-10-09 13:27:22 -0300

    Subindo a aplicação e criando routers

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const routerPrincipal = require('./routes/principal');
+const routerPedidos = require('./routes/pedidos');
+const routerPerfil = require('./routes/perfil');
+
+
+const app = express();
+const porta = 3000;
+
+// Configurando ejs
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+// routers
+app.use('/perfil', routerPerfil);
+app.use('/pedidos', routerPedidos);
+app.use('/', routerPrincipal);
+
+app.listen(porta, () => {
+    console.log('Servidor ouvindo na porta ' + porta);
+});
+
